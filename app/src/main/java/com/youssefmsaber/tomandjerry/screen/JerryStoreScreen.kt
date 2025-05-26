a58d4240fafb646566f4e1464279d2015e8ce2fd
Youssef Mohamed <youssef.mu.saber@gmail.com>
2025-05-26 19:55:37 +0300

    Initial commit

---
+++ app/src/main/java/com/youssefmsaber/tomandjerry/screen/JerryStoreScreen.kt
@@ -0,0 +1,15 @@
+package com.youssefmsaber.tomandjerry.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun JerryStoreScreen() {
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun JerryStoreScreenPreview() {
+
+}
---
+++ app/src/main/java/com/youssefmsaber/tomandjerry/screen/TomKitchenScreen.kt
@@ -0,0 +1,15 @@
+package com.youssefmsaber.tomandjerry.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TomKitchenScreen() {
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun TomKitchenScreenPreview() {
+
+}
