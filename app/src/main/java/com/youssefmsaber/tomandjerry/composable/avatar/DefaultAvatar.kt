aa5932c059ab2cf59cb92d96eb1f4a1f9873a9c6
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-05-30 15:27:09 +0300

    Created the default avatar composable

---
+++ app/src/main/java/com/youssefmsaber/tomandjerry/composable/avatar/DefaultAvatar.kt
@@ -0,0 +1,30 @@
+package com.youssefmsaber.tomandjerry.composable.avatar
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.youssefmsaber.tomandjerry.R
+import com.youssefmsaber.tomandjerry.ui.theme.Radius12
+
+@Composable
+fun DefaultAvatar(
+    image: Painter,
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = image,
+        contentDescription = "Default Avatar",
+        modifier = modifier.clip(RoundedCornerShape(Radius12))
+    )
+}
+
+@Preview
+@Composable
+fun DefaultAvatarPreview() {
+    DefaultAvatar(image = painterResource(R.drawable.profile_image))
+}
