dfe615fa1e0682e4c822475a325e6f434628a5c7
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-05-30 17:48:00 +0300

    Created Horizontal Spacers

---
+++ app/src/main/java/com/youssefmsaber/tomandjerry/composable/spacer/HorizontalSpacer8.kt
@@ -0,0 +1,12 @@
+package com.youssefmsaber.tomandjerry.composable.spacer
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.youssefmsaber.tomandjerry.ui.theme.Padding8
+
+@Composable
+fun HorizontalSpacer8() {
+    Spacer(modifier = Modifier.width(Padding8))
+}
