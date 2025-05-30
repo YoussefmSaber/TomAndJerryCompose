24a819e49b1314c4ea789c2370b224c0fa391b0c
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-05-30 19:07:06 +0300

    Added IBM_Plex fonts

---
+++ app/src/main/java/com/youssefmsaber/tomandjerry/ui/theme/Fonts.kt
@@ -0,0 +1,16 @@
+package com.youssefmsaber.tomandjerry.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.youssefmsaber.tomandjerry.R
+
+val IBM_Plex = FontFamily(
+    Font(R.font.ibmplexsansarabic_bold, FontWeight.Bold),
+    Font(R.font.ibmplexsansarabic_semibold, FontWeight.SemiBold),
+    Font(R.font.ibmplexsansarabic_medium, FontWeight.Medium),
+    Font(R.font.ibmplexsansarabic_regular, FontWeight.Normal),
+    Font(R.font.ibmplexsansarabic_light, FontWeight.Light),
+    Font(R.font.ibmplexsansarabic_extralight, FontWeight.ExtraLight),
+    Font(R.font.ibmplexsansarabic_thin, FontWeight.Thin),
+)
