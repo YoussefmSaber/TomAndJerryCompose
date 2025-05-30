d366c03d2a82b9be4087029fc86a7b5d655f4362
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-05-30 15:28:25 +0300

    Created a Dimens file that contains all the measurements and sizes

---
+++ app/src/main/java/com/youssefmsaber/tomandjerry/ui/theme/Dimens.kt
@@ -0,0 +1,20 @@
+package com.youssefmsaber.tomandjerry.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val Padding4 = 4.dp
+val Padding8 = 8.dp
+val Padding16 = 16.dp
+
+val StrokeWidth1 = 1.dp
+val StrokeWidth2 = 2.dp
+
+val OffsetNegative4 = (-4).dp
+val OffsetNegative8 = (-8).dp
+val OffsetNegative16 = (-16).dp
+
+
+val Radius8 = 8.dp
+val Radius12 = 12.dp
+val Radius16 = 16.dp
+val Radius24 = 24.dp
