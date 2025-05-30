94c7d064be01e3e68750b3ad6a8a40258f1549e2
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-05-30 18:24:04 +0300

    Created the price card and cart button

---
+++ app/src/main/java/com/youssefmsaber/tomandjerry/composable/button/CartButton.kt
@@ -0,0 +1,48 @@
+package com.youssefmsaber.tomandjerry.composable.button
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.youssefmsaber.tomandjerry.R
+import com.youssefmsaber.tomandjerry.ui.theme.Padding8
+import com.youssefmsaber.tomandjerry.ui.theme.PrimaryColor
+import com.youssefmsaber.tomandjerry.ui.theme.Radius8
+import com.youssefmsaber.tomandjerry.ui.theme.StrokeWidth1
+
+@Composable
+fun CartButton(
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = { },
+        modifier = modifier.border(
+            width = StrokeWidth1,
+            color = PrimaryColor,
+            shape = RoundedCornerShape(Radius8)
+        ).size(30.dp)
+    ) {
+        Icon(
+            painter = painterResource(R.drawable.add_to_cart_icon),
+            contentDescription = "Add to cart icon",
+            tint = PrimaryColor,
+            modifier = modifier
+                .padding(Padding8)
+                .fillMaxSize()
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CartButtonPreview() {
+    CartButton()
+}
---
+++ app/src/main/java/com/youssefmsaber/tomandjerry/composable/card/PriceCard.kt
@@ -0,0 +1,87 @@
+package com.youssefmsaber.tomandjerry.composable.card
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.youssefmsaber.tomandjerry.R
+import com.youssefmsaber.tomandjerry.composable.spacer.HorizontalSpacer4
+import com.youssefmsaber.tomandjerry.ui.theme.Padding8
+import com.youssefmsaber.tomandjerry.ui.theme.PriceContainerColor
+import com.youssefmsaber.tomandjerry.ui.theme.PrimaryColor
+import com.youssefmsaber.tomandjerry.ui.theme.Radius8
+
+@Composable
+fun PriceCard(
+    oldPrice: String,
+    newPrice: String,
+    modifier: Modifier = Modifier
+) {
+    val price = buildAnnotatedString {
+        withStyle(style = SpanStyle(fontSize = 12.sp)) {
+            if (newPrice.isNotBlank()) {
+                withStyle(
+                    style = SpanStyle(
+                        textDecoration = TextDecoration.LineThrough,
+                    )
+                ) {
+                    append(oldPrice)
+                }
+                append(" ")
+                append(newPrice)
+            } else {
+                append(oldPrice)
+            }
+            append(" ")
+            append("cheeses")
+        }
+    }
+
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = PriceContainerColor,
+            contentColor = PrimaryColor
+        ),
+        shape = RoundedCornerShape(Radius8),
+        modifier = modifier.height(30.dp)
+    ) {
+        Row(
+            modifier = modifier.padding(Padding8),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.money_bag_icon),
+                contentDescription = "Price icon",
+                tint = PrimaryColor,
+                modifier = modifier.size(16.dp)
+            )
+            HorizontalSpacer4()
+            Text(price)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PriceCardPreview() {
+    PriceCard(
+        oldPrice = 0.toString(),
+        newPrice = "2"
+    )
+}
