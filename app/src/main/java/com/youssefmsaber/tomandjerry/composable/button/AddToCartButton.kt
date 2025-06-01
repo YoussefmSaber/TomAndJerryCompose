9189025bf7470f89ede1de48f63520243dea084e
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-06-01 17:38:54 +0300

    Extracted the add to cart button to be used in other components

---
+++ app/src/main/java/com/youssefmsaber/tomandjerry/composable/button/AddToCartButton.kt
@@ -0,0 +1,102 @@
+package com.youssefmsaber.tomandjerry.composable.button
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.youssefmsaber.tomandjerry.composable.spacer.HorizontalSpacer8
+import com.youssefmsaber.tomandjerry.ui.theme.ButtonColor
+import com.youssefmsaber.tomandjerry.ui.theme.IBM_Plex
+import com.youssefmsaber.tomandjerry.ui.theme.LightWhite
+import com.youssefmsaber.tomandjerry.ui.theme.Radius16
+
+@Composable
+fun AddToCartButton() {
+    Button(
+        onClick = { },
+        shape = RoundedCornerShape(Radius16),
+        colors = ButtonDefaults.buttonColors(containerColor = ButtonColor),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                "Add to cart",
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    fontFamily = IBM_Plex,
+                    fontWeight = FontWeight.Medium
+                )
+            )
+            HorizontalSpacer8()
+            Canvas(
+                modifier = Modifier
+                    .size(5.dp) // diameter = 2 * radius
+                    .align(Alignment.CenterVertically)
+            ) {
+                drawCircle(
+                    color = LightWhite,
+                    radius = size.minDimension / 2
+                )
+            }
+            HorizontalSpacer8()
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    "3 cheeses",
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 14.sp,
+                        fontFamily = IBM_Plex,
+                        fontWeight = FontWeight.Medium,
+                        letterSpacing = (0.5).sp,
+                        lineHeight = 16.sp
+                    )
+                )
+                Text(
+                    "5 cheeses",
+                    style = TextStyle(
+                        color = Color.White.copy(alpha = 0.8f),
+                        fontSize = 12.sp,
+                        fontFamily = IBM_Plex,
+                        fontWeight = FontWeight.Medium,
+                        letterSpacing = (0.5).sp,
+                        lineHeight = 16.sp,
+                        textDecoration = TextDecoration.LineThrough
+                    )
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AddToCartButtonPreview() {
+    AddToCartButton()
+}
--- app/src/main/java/com/youssefmsaber/tomandjerry/screen/TomKitchenScreen.kt
+++ app/src/main/java/com/youssefmsaber/tomandjerry/screen/TomKitchenScreen.kt
@@ -1,6 +1,5 @@
 package com.youssefmsaber.tomandjerry.screen
 
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,14 +9,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -29,12 +25,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.youssefmsaber.tomandjerry.R
+import com.youssefmsaber.tomandjerry.composable.button.AddToCartButton
 import com.youssefmsaber.tomandjerry.composable.card.DetailsCard
 import com.youssefmsaber.tomandjerry.composable.card.MealPriceCard
 import com.youssefmsaber.tomandjerry.composable.card.PreparationMethodCard
@@ -47,12 +43,10 @@ import com.youssefmsaber.tomandjerry.composable.spacer.VerticalSpacer46
 import com.youssefmsaber.tomandjerry.composable.spacer.VerticalSpacer8
 import com.youssefmsaber.tomandjerry.data.detailsItems
 import com.youssefmsaber.tomandjerry.data.preparationItems
-import com.youssefmsaber.tomandjerry.ui.theme.ButtonColor
 import com.youssefmsaber.tomandjerry.ui.theme.DarkGrayColor
 import com.youssefmsaber.tomandjerry.ui.theme.DetailsBackgroundColor
 import com.youssefmsaber.tomandjerry.ui.theme.IBM_Plex
 import com.youssefmsaber.tomandjerry.ui.theme.LightBLueBackgroundColor
-import com.youssefmsaber.tomandjerry.ui.theme.LightWhite
 import com.youssefmsaber.tomandjerry.ui.theme.LighterDarkGrayColor
 import com.youssefmsaber.tomandjerry.ui.theme.Padding16
 import com.youssefmsaber.tomandjerry.ui.theme.Padding24
@@ -258,70 +252,7 @@ private fun BottomBar(modifier: Modifier = Modifier) {
         Column(
             modifier = Modifier.padding(Padding16)
         ) {
-            Button(
-                onClick = { },
-                shape = RoundedCornerShape(Radius16),
-                colors = ButtonDefaults.buttonColors(containerColor = ButtonColor),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(56.dp)
-            ) {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text(
-                        "Add to cart",
-                        style = TextStyle(
-                            color = Color.White,
-                            fontSize = 16.sp,
-                            fontFamily = IBM_Plex,
-                            fontWeight = FontWeight.Medium
-                        )
-                    )
-                    HorizontalSpacer8()
-                    Canvas(
-                        modifier = Modifier
-                            .size(5.dp) // diameter = 2 * radius
-                            .align(Alignment.CenterVertically)
-                    ) {
-                        drawCircle(
-                            color = LightWhite,
-                            radius = size.minDimension / 2
-                        )
-                    }
-                    HorizontalSpacer8()
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(
-                            "3 cheeses",
-                            style = TextStyle(
-                                color = Color.White,
-                                fontSize = 14.sp,
-                                fontFamily = IBM_Plex,
-                                fontWeight = FontWeight.Medium,
-                                letterSpacing = (0.5).sp,
-                                lineHeight = 16.sp
-                            )
-                        )
-                        Text(
-                            "5 cheeses",
-                            style = TextStyle(
-                                color = Color.White.copy(alpha = 0.8f),
-                                fontSize = 12.sp,
-                                fontFamily = IBM_Plex,
-                                fontWeight = FontWeight.Medium,
-                                letterSpacing = (0.5).sp,
-                                lineHeight = 16.sp,
-                                textDecoration = TextDecoration.LineThrough
-                            )
-                        )
-                    }
-                }
-            }
+            AddToCartButton()
         }
     }
 }
