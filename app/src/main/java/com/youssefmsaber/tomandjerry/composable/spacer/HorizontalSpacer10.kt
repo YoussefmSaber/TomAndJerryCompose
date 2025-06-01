aa627910a4078ad88843f1b23f49b768ed0228c4
Youssef Mohammed <youssef.mu.saber@gmail.com>
2025-06-01 20:30:36 +0300

    Added new spacers

---
+++ app/src/main/java/com/youssefmsaber/tomandjerry/composable/spacer/HorizontalSpacer10.kt
@@ -0,0 +1,12 @@
+package com.youssefmsaber.tomandjerry.composable.spacer
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.youssefmsaber.tomandjerry.ui.theme.Padding10
+
+@Composable
+fun HorizontalSpacer10() {
+    Spacer(modifier = Modifier.width(Padding10))
+}
---
+++ app/src/main/java/com/youssefmsaber/tomandjerry/composable/spacer/HorizontalSpacer2.kt
@@ -0,0 +1,12 @@
+package com.youssefmsaber.tomandjerry.composable.spacer
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.youssefmsaber.tomandjerry.ui.theme.Padding2
+
+@Composable
+fun HorizontalSpacer2() {
+    Spacer(modifier = Modifier.width(Padding2))
+}
